6919cb479779df111396cf0eb9eff5f383926d8c
TheShark34 <adrien1975@live.fr>
2015-04-25 16:14:22 +0200

    Tabluation fixes

--- installer.php
+++ installer.php
@@ -1,88 +1,88 @@
 <?php
 
-	/*
-	 * Copyright 2015 TheShark34
-	 *
-	 * This file is part of S-Update.
+    /*
+     * Copyright 2015 TheShark34
+     *
+     * This file is part of S-Update.
 
-	 * S-Update is free software: you can redistribute it and/or modify
-	 * it under the terms of the GNU Lesser General Public License as published by
-	 * the Free Software Foundation, either version 3 of the License, or
-	 * (at your option) any later version.
-	 *
-	 * S-Update is distributed in the hope that it will be useful,
-	 * but WITHOUT ANY WARRANTY; without even the implied warranty of
-	 * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	 * GNU Lesser General Public License for more details.
-	 *
-	 * You should have received a copy of the GNU Lesser General Public License
-	 * along with S-Update.  If not, see <http://www.gnu.org/licenses/>.
-	 */
+     * S-Update is free software: you can redistribute it and/or modify
+     * it under the terms of the GNU Lesser General Public License as published by
+     * the Free Software Foundation, either version 3 of the License, or
+     * (at your option) any later version.
+     *
+     * S-Update is distributed in the hope that it will be useful,
+     * but WITHOUT ANY WARRANTY; without even the implied warranty of
+     * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+     * GNU Lesser General Public License for more details.
+     *
+     * You should have received a copy of the GNU Lesser General Public License
+     * along with S-Update.  If not, see <http://www.gnu.org/licenses/>.
+     */
 
-	$VERSION="2.0.0-SNAPSHOT";
+    $VERSION="2.0.0-SNAPSHOT";
 
-	if(isset($_GET['request']))
-		if($_GET['request'] == 'download')
-			download();
-		else
-			home();
-	else
-		home();
+    if(isset($_GET['request']))
+        if($_GET['request'] == 'download')
+            download();
+        else
+            home();
+    else
+        home();
 
 
-	function download() {
-		echo "success";
-	}
+    function download() {
+        echo "success";
+    }
 
-	function home() {
+    function home() {
 
 ?>
 
 <!DOCTYPE html>
 <html>
-	<head>
-		<meta charset="utf-8">
-    	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-   		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>S-Update - Acceuil</title>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>S-Update - Acceuil</title>
 
-		<!-- Bootstrap -->
-    	<link href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet">
+        <!-- Bootstrap -->
+        <link href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet">
 
-		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-	    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-	    <!--[if lt IE 9]>
-	      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-	      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-	    <![endif]-->
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+          <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+          <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->
 
-	    <link href="http://theshark34.github.io/S-Update-Server/style.css" rel="stylesheet">
-	</head>
+        <link href="http://theshark34.github.io/S-Update-Server/style.css" rel="stylesheet">
+    </head>
 
-	<body>
-		<div class='fulldiv'>
-			<div class="center">
-				<img src="http://theshark34.github.io/S-Update-Server/logo.png" />
-			</div>
+    <body>
+        <div class='fulldiv'>
+            <div class="center">
+                <img src="http://theshark34.github.io/S-Update-Server/logo.png" />
+            </div>
 
-	    	<div class="progress">
-			    <div id="pb" class="progress-bar progress-bar-custom" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
-			</div>
-		</div>
+            <div class="progress">
+                <div id="pb" class="progress-bar progress-bar-custom" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
+        </div>
 
-		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-    	<!-- Include all compiled plugins (below), or include individual files as needed -->
-    	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
-    	<!-- Offical S-Update install script -->
-    	<script src="http://theshark34.github.io/S-Update-Server/installer.js"></script>
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+        <!-- Offical S-Update install script -->
+        <script src="http://theshark34.github.io/S-Update-Server/installer.js"></script>
 
-    	<!-- Starting Downloader -->
-    	<script> sendInstallerRequest("download", startInstallation); </script>
-	</body>
+        <!-- Starting Downloader -->
+        <script> sendInstallerRequest("download", startInstallation); </script>
+    </body>
 </html> 
 
 <?php
 
-	}
+    }
 ?>
