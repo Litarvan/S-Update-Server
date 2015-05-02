6dba6ab2ec706c506309689ac5a3a2224e23d2eb
TheShark34 <adrien1975@live.fr>
2015-05-02 20:23:07 +0200

    Fixed a crash

--- server/server.php
+++ server/server.php
@@ -76,31 +76,22 @@
 	}
 
 	function get_ip_address() {
-    if (!empty($_SERVER['HTTP_CLIENT_IP']) && validate_ip($_SERVER['HTTP_CLIENT_IP']))
-        return $_SERVER['HTTP_CLIENT_IP'];
+	    $ip_keys = array('HTTP_CLIENT_IP', 'HTTP_X_FORWARDED_FOR', 'HTTP_X_FORWARDED', 'HTTP_X_CLUSTER_CLIENT_IP', 'HTTP_FORWARDED_FOR', 'HTTP_FORWARDED', 'REMOTE_ADDR');
+	    foreach ($ip_keys as $key)
+	        if (array_key_exists($key, $_SERVER) === true)
+	            foreach (explode(',', $_SERVER[$key]) as $ip) {
+	                $ip = trim($ip);
+	                if (validate_ip($ip))
+	                    return $ip;
+	            }
 
-    if (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))
-      if (strpos($_SERVER['HTTP_X_FORWARDED_FOR'], ',') !== false) {
-      	$iplist = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
-        foreach ($iplist as $ip)
-          if (validate_ip($ip))
-            return $ip;
-      } elseif (validate_ip($_SERVER['HTTP_X_FORWARDED_FOR']))
-              return $_SERVER['HTTP_X_FORWARDED_FOR'];
+	    return isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : "Unknown";
+	}
 
-    if(!empty($_SERVER['HTTP_X_FORWARDED']) && validate_ip($_SERVER['HTTP_X_FORWARDED']))
-      return $_SERVER['HTTP_X_FORWARDED'];
-
-    if(!empty($_SERVER['HTTP_X_CLUSTER_CLIENT_IP']) && validate_ip($_SERVER['HTTP_X_CLUSTER_CLIENT_IP']))
-      return $_SERVER['HTTP_X_CLUSTER_CLIENT_IP'];
-
-    if(!empty($_SERVER['HTTP_FORWARDED_FOR']) && validate_ip($_SERVER['HTTP_FORWARDED_FOR']))
-      return $_SERVER['HTTP_FORWARDED_FOR'];
-
-    if(!empty($_SERVER['HTTP_FORWARDED']) && validate_ip($_SERVER['HTTP_FORWARDED']))
-      return $_SERVER['HTTP_FORWARDED'];
-
-    return $_SERVER['REMOTE_ADDR'];
+	function validate_ip($ip) {
+	    if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4 | FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE) === false)
+	        return false;
+	    return true;
 	}
 
 
